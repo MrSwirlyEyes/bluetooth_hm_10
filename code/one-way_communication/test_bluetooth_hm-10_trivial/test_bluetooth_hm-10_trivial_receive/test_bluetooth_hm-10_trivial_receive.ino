136ba0780a209d207e5be9c9a086ad549aaa8ae7
Colin Keef <MrSwirlyEyes@MrSwirlyEyes.local>
2018-06-27 23:24:57 -0700

    Update trivial test code (RX side).

--- code/one-way_communication/test_bluetooth_hm-10_trivial/test_bluetooth_hm-10_trivial_receive/test_bluetooth_hm-10_trivial_receive.ino
+++ code/one-way_communication/test_bluetooth_hm-10_trivial/test_bluetooth_hm-10_trivial_receive/test_bluetooth_hm-10_trivial_receive.ino
@@ -1,18 +1,46 @@
+// Basic serial communication with Bluetooth HM-10
+// Receive a trivial string through the Bluetooth HM-10
+//
+//  Arduino to HM-10 connections
+//  Arduino pin 2 (TX) to voltage divider then to HM-10 RX
+//  Arduino pin 3 to HM-10 TX
+//  Connect GND from the Arduiono to GND on the HM-10
+//
+// When a command is entered in to the serial monitor on the computer 
+// the Arduino will relay the command to the HM-10
+
+
+// Library to make a Software UART
 #include <SoftwareSerial>;
+
+#define RX 3
+#define TX 2
+
+#define BAUDRATE 9600
+
+char c = ' ';
  
-SoftwareSerial BTSerial(10, 11); // TX | RX
+SoftwareSerial BTSerial(RX, TX); // (RX, TX)
  
 void setup() {
-  Serial.begin(9600);
-  BTSerial.begin(9600);  // HM-10 default speed in AT command more
+
+  // Start Serial Monitor for feedback
+  Serial.begin(BAUDRATE);
+
+  // HM-10 default speed in AT command mode
+  BTSerial.begin(BAUDRATE);
 }
  
 void loop() {
-  // Keep reading from HM-10 and send to Arduino Serial Monitor
-  char rc;
+  
+  // Keep reading from HM-10 and send to Arduino Serial Monitor  
   while(BTSerial.available() > 0) {
-  rc = BTSerial.read();
-  Serial.println(rc);
+
+    // Read from the bluetooth buffer
+    c = BTSerial.read();
+
+    // Write the received bluetooth data to the Serial Monitor
+    Serial.println(c);
   }
   delay(1000);
 }
